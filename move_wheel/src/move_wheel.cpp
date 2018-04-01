fa442aa9db9dec0d37cbd7aa7ec0fa69476a95e2
EdisonGuo <edisonguocat@gmail.com>
2018-04-01 23:09:29 +0800

    fix

--- move_wheel/src/move_wheel.cpp
+++ move_wheel/src/move_wheel.cpp
@@ -17,14 +17,23 @@ void turnLeft(int pin1_right, int pin2_right){
 	wheel(pin1_right,pin2_right);
 
 }
-void turnRght(int pin1_left, int pin2_left){
+void turnRight(int pin1_left, int pin2_left){
 
 	wheel(pin1_left,pin2_left);
+
+}
+void brake(int pin1_right, int pin2_right, int pin1_left, int pin2_left){
+
+	digitalWrite(pin1_right,LOW);
+	digitalWrite(pin2_right,LOW);
+	digitalWrite(pin1_left,LOW);
+	digitalWrite(pin2_left,LOW);
 
 }
 void wheel(int pin1, int pin2){
 
 	digitalWrite(pin1,HIGH);
 	digitalWrite(pin2,LOW);
-
+	analogWrite(pin1,120);
+	analogWrite(pin2,0);
 }
